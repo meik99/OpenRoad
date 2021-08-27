b800986344c712e7729768dde8b83489d8a74965
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-08-27 19:09:13 +0200

    implemented compass rotating position marker to direction

---
+++ app/src/main/java/com/rynkbit/openroad/map/Compass.kt
@@ -0,0 +1,46 @@
+package com.rynkbit.openroad.map
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+
+
+class Compass(context: Context): SensorEventListener {
+    fun interface CompassListener {
+        fun onRotation(orientation: Float)
+    }
+
+    val listeners = mutableListOf<CompassListener>()
+
+    private val sensorManager: SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val rotation: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+
+    private var r = FloatArray(9)
+    private var orientation = FloatArray(3)
+
+    fun onResume() {
+        sensorManager.registerListener(this, rotation, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    fun onPause() {
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.apply {
+            if (sensor.type == Sensor.TYPE_ROTATION_VECTOR) {
+                SensorManager.getRotationMatrixFromVector(r, values)
+                val orientation = SensorManager.getOrientation(r, orientation)
+                val rotation = -SensorManager.getOrientation(r, orientation)[0] * 360 / (2 * 3.14159f)
+
+                for (listener in listeners) {
+                    listener.onRotation(rotation)
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) { }
+}
--- app/src/main/java/com/rynkbit/openroad/map/Map.kt
+++ app/src/main/java/com/rynkbit/openroad/map/Map.kt
@@ -3,9 +3,9 @@ package com.rynkbit.openroad.map
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.LocationListener
-import android.location.LocationManager
+import android.hardware.*
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,10 +15,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceManager
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.CancellationToken
-import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.location.*
 import com.rynkbit.openroad.R
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -26,20 +23,27 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
-import java.util.concurrent.Executors
+import org.osmdroid.views.overlay.compass.CompassOverlay
 
 
 class Map : Fragment() {
+    companion object {
+        val TAG = Map::class.java.simpleName
+    }
+
     private lateinit var viewModel: MapViewModel
     private lateinit var map: MapView
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var marker: Marker
+    private lateinit var compass: Compass
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Configuration.getInstance().load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
+        Configuration.getInstance()
+            .load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
         return inflater.inflate(R.layout.map_fragment, container, false)
     }
 
@@ -54,49 +58,121 @@ class Map : Fragment() {
         map.controller.setZoom(10.0)
         map.controller.setCenter(GeoPoint(48.8583, 2.2944))
 
+        marker = Marker(map)
+        map.overlays.add(marker)
+
+        compass = Compass(requireContext())
+        compass.listeners.add(Compass.CompassListener {
+            marker.rotation = it
+            map.invalidate()
+        })
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                val point = GeoPoint(
+                    locationResult.lastLocation.latitude,
+                    locationResult.lastLocation.longitude
+                )
+
+                marker.position = point
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+                marker.icon = ResourcesCompat.getDrawable(
+                    resources,
+                    R.drawable.ic_baseline_expand_less_24,
+                    requireActivity().theme
+                )
+
+                map.controller.setZoom(19.0)
+                map.controller.setCenter(point)
+                map.invalidate()
+            }
+
+            override fun onLocationAvailability(locationAvailability: LocationAvailability) {
+                super.onLocationAvailability(locationAvailability)
+            }
+        }
+
+        locationRequest = LocationRequest.create()
+        locationRequest.interval = 500
+        locationRequest.fastestInterval = 100
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
         askRequiredPermissions(getRequiredPermissions())
     }
 
     override fun onResume() {
         super.onResume()
         map.onResume()
+        compass.onResume()
     }
 
     override fun onPause() {
         super.onPause()
         map.onPause()
+        compass.onPause()
     }
 
 
     private fun setInitialLocation() {
+        val fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(requireContext())
         try {
-            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-            fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token).addOnSuccessListener {
-                val point = GeoPoint(it.latitude, it.longitude)
-                val marker = Marker(map)
-
-                marker.position = point
-                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_TOP)
-                marker.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_expand_less_24, requireActivity().theme)
-
-                map.overlays.add(marker)
-                map.controller.setCenter(point)
-                map.controller.setZoom(19.0)
-                map.postInvalidate()
-                map.invalidate()
-            }
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
         } catch (e: SecurityException) {
-            Log.e(Map::class.java.simpleName, e.message ?: "")
+            Log.e(TAG, "setInitialLocation: ${e.message}", e)
         }
+//        try {
+//            val fusedLocationClient =
+//                LocationServices.getFusedLocationProviderClient(requireContext())
+//            fusedLocationClient.requestLocationUpdates(
+//                LocationRequest.create(),
+//                object : LocationCallback() {
+//                    override fun onLocationResult(locationResult: LocationResult) {
+//                        super.onLocationResult(locationResult)
+//
+//                        val point = GeoPoint(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
+//                        val marker = Marker(map)
+//
+//
+//                        marker.position = point
+//                        marker.rotation = locationResult.lastLocation.bearing
+//                        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+//                        marker.icon = ResourcesCompat.getDrawable(
+//                            resources,
+//                            R.drawable.ic_baseline_expand_less_24,
+//                            requireActivity().theme
+//                        )
+//
+//                        map.overlays.add(marker)
+//                        map.controller.setZoom(19.0)
+//                        map.controller.setCenter(point)
+//                        map.postInvalidate()
+//                        map.invalidate()
+//                    }
+//                },
+//                Looper.getMainLooper()
+//            )
+//
+//        } catch (e: SecurityException) {
+//            Log.e(Map::class.java.simpleName, e.message ?: "")
+//        }
     }
 
     private fun askRequiredPermissions(requiredPermissions: List<String>) {
         if (requiredPermissions.isNotEmpty()) {
-            val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-                if (isGranted) {
-                    setInitialLocation()
+            val requestPermissionLauncher =
+                registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                    if (isGranted) {
+                        setInitialLocation()
+                    }
                 }
-            }
+
             for (permission in requiredPermissions) {
                 requestPermissionLauncher.launch(permission)
             }
@@ -109,13 +185,13 @@ class Map : Fragment() {
         val context = requireContext()
         val permissions = mutableListOf<String>()
 
-        if (context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)  {
+        if (context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
         }
-        if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)  {
+        if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
         }
-        if (context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)  {
+        if (context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
 
